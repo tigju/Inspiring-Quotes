41efdf1c877a18e3123fc68d2f88e34985e567ae
unknown <tigju24@gmail.com>
2016-11-27 19:15:48 -0500

    fix url

--- js.js
+++ js.js
@@ -11,7 +11,7 @@ $('#quote-button').on('click', function(e) {
   $('#quote-button').text('Change Quote');
     e.preventDefault();
     $.ajax( {
-      url: 'http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
+      url: 'https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1',
       success: function(data) {
         var post = data.shift(); // The data is an array of posts. Grab the first one.
         var dec = decodeEntities(post.content);
